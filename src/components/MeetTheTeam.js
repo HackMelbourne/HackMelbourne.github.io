7c09dbaaac94fbf1cc43b895c71b9b8c10b781a2
Matthew Kwon <mink9909@gmail.com>
2020-10-03 14:59:53 +1000

    v 0.8

--- src/components/MeetTheTeam.js
+++ src/components/MeetTheTeam.js
@@ -1,11 +1,31 @@
 import '../styles/meetTheTeam.css';
 import React from "react";
+import chris from "../img/team/hm-chris.jpg";
+import fiona from "../img/team/hm-fiona.jpg";
+import jeeng from "../img/team/hm-jeeng.png";
+import jenny from "../img/team/hm-jenny.jpg";
+import joanne from "../img/team/hm-joanne.jpg";
+import kye from "../img/team/hm-kye.jpg";
+import ryan from "../img/team/hm-ryan.png";
+import serena from "../img/team/hm-serena.jpg";
+import victoria from "../img/team/hm-victoria.jpg";
 
 function MeetTheTeam() {
   return (
-    <div className="App">
+    <div>
       <div className="meetTheTeamContainer">
         <h1 className="meetTheTeamTitle">Meet the team</h1>
+        <div className="teamPhotos">
+          <img className="profile" src={chris}></img>
+          <img className="profile" src={fiona}></img>
+          <img className="profile" src={jeeng}></img>
+          <img className="profile" src={jenny}></img>
+          <img className="profile" src={joanne}></img>
+          <img className="profile" src={kye}></img>
+          <img className="profile" src={ryan}></img>
+          <img className="profile" src={serena}></img>
+          <img className="profile" src={victoria}></img>
+        </div>
       </div>
     </div>
   );
--- src/components/WhatWeDo.js
+++ src/components/WhatWeDo.js
@@ -7,19 +7,21 @@ import EducationProgram from '../img/EducationProgram.png';
 function WhatWeDo() {
   return (
     <div className="whatWeDoContainer">
-      <h1>What we are doing</h1>
-      <div className="whatWeDoPrograms">
-        <div className="program">
-          <img src={MainHackathon}></img>
-          <p>HackMelbourne Hackathon</p>
-        </div>
-        <div className="program">
-          <img src={RookieHackathon}></img>
-          <p>Rookie Hackathon</p>
-        </div>
-        <div className="program">
-          <img src={EducationProgram}></img>
-          <p>Education Program</p>
+      <div className="homeContent">
+        <h1>What we are doing</h1>
+        <div className="whatWeDoPrograms">
+          <div className="program">
+            <img className="programImg"src={MainHackathon}></img>
+            <p>HackMelbourne Hackathon</p>
+          </div>
+          <div className="program">
+            <img className="programImg" src={RookieHackathon}></img>
+            <p>Rookie Hackathon</p>
+          </div>
+          <div className="program">
+            <img className="programImg" src={EducationProgram}></img>
+            <p>Education Program</p>
+          </div>
         </div>
       </div>
     </div>
