6c783c3c67660e87c35c3558a06bdcaf99b67d89
Matthew Kwon <mink9909@gmail.com>
2020-11-26 17:53:50 +1100

    changed colour theme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/navbar.css';
-import Logo from '../img/H{Logo.png'
+import Logo from '../img/H{Logo.png';
+import Hamburger from '../img/hamburger.png'
 
 const Navbar = () => {
   return (
@@ -12,9 +13,10 @@ const Navbar = () => {
         </div>
         <nav className="navigation">
             <ul>
+                <li><a></a></li>
                 <li><a href="/initiatives">Initatives</a></li>
                 <li><a href="/sponsors">Sponsor Us</a></li>
-                <li><a class="highlight-button" target="_blank" href="https://forms.gle/yBCTHgWdBj66JhUV6">Sign Up</a></li>
+                <li><a class="signup-button" target="_blank" href="https://forms.gle/yBCTHgWdBj66JhUV6">Sign Up</a></li>
             </ul>
         </nav>
     </header>
