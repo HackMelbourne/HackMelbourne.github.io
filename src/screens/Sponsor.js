774150f186ff487d531f2b03047cc53800711e6e
Matthew Kwon <mink9909@gmail.com>
2020-12-08 23:29:29 +1100

    changed alot of styling shit

--- src/screens/Sponsor.js
+++ src/screens/Sponsor.js
@@ -1,6 +1,6 @@
 import '../styles/sponsor.css';
 import React from "react";
-import Challenge5 from '../Challenge5.pdf';
+import HackMelbourne_Prospectus from '../HackMelbourne_Prospectus.pdf';
 
 function Sponsor() {
   return (
@@ -9,7 +9,7 @@ function Sponsor() {
       <p>Organising large scale, cross-university hackathons requires considerable effort and we couldn’t do it without your support! </p>
       <h3>What's in it for you?</h3>
       <p className="pushButtonDown">Marketing, promotion, invitation to our events and access to a talent pool of highly skilled software developers and programmers are just some of the reasons to support us.</p>
-      <a className="highlight-button" href={Challenge5} download="HackMelbourne_Sponsorship_Prospectus.pdf"> Download sponsorship prospectus </a>
+      <a className="highlight-button" href={HackMelbourne_Prospectus} download="HackMelbourne_Sponsorship_Prospectus.pdf"> Download sponsorship prospectus </a>
       <h3 className="pushButtonUp">Interested?</h3>
       <p>Email us at <u><a href="mailto:hackmelb.team@gmail.com">hackmelb.team@gmail.com</a></u></p>
     </div>
