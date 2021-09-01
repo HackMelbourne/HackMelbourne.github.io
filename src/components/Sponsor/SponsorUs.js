f09ba7af6722f3fee5a109fb8c41064dbb7fb306
ilaylow <chuenleylow@gmail.com>
2021-09-01 21:12:09 +1000

    removed prospectus

--- src/components/Sponsor/SponsorUs.js
+++ src/components/Sponsor/SponsorUs.js
@@ -11,7 +11,7 @@ function SponsorUs() {
       <img src={hackmelb_team} className="teamPhoto"></img>
       <h3>What's in it for you?</h3>
       <p className="pushButtonDown">Marketing, promotion, invitation to our events and access to a talent pool of highly skilled software developers and programmers are just some of the reasons to support us.</p>
-      <a className="highlight-button" href={HackMelbourne_Prospectus} download="HackMelbourne_Sponsorship_Prospectus.pdf"> Download sponsorship prospectus </a>
+      {/* <a className="highlight-button" href={HackMelbourne_Prospectus} download="HackMelbourne_Sponsorship_Prospectus.pdf"> Download sponsorship prospectus </a> */}
       <h3 className="pushButtonUp">Interested?</h3>
       <p>Email us at : <u><a href="mailto:sponsorship@hackmelb.org">sponsorship@hackmelb.org</a></u></p>
     </div>
