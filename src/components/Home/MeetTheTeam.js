8e209353edf08a4972c001c10dba80d0cb1e39b3
Xin Yu <66938425+chuahxinyu@users.noreply.github.com>
2022-03-02 12:31:23 +1100

    Updated the Team

--- src/components/Home/MeetTheTeam.js
+++ src/components/Home/MeetTheTeam.js
@@ -1,30 +1,55 @@
 import '../../styles/meetTheTeam.css';
 import React from "react";
-import ali from "../../img/team/hm-ali-m.jpg";
-import apuroop from "../../img/team/hm-apuroop-c.JPG"
-import chris from "../../img/team/hm-chris-l.jpg";
-import daniel from "../../img/team/hm-daniel-m.PNG";
-import deborah from "../../img/team/hm-deborah-l.jpg";
-import fiona from "../../img/team/hm-fiona-z.jpg";
-import ivan from "../../img/team/hm-ivan-b.png";
-import jeeng from "../../img/team/hm-jeeng-l.png";
-import jenny from "../../img/team/hm-jenny-w.jpg";
-import joanne from "../../img/team/hm-joanne-n.jpg";
-import jonathan from "../../img/team/hm-jonathan-j.jpg";
-import keith from "../../img/team/hm-keith-l.png";
-import kye from "../../img/team/hm-kye-c.jpg";
-import leo from "../../img/team/hm-leo-h.jpg";
-import ley from "../../img/team/hm-ley-l.jpg";
-import matthew from "../../img/team/hm-matthew-k.jpg";
-import muskaan from "../../img/team/hm-muskaan-d.jpeg";
-import patricia from "../../img/team/hm-patricia-w.jpg";
-import richard from "../../img/team/hm-richard-v.jpg";
-import ryan from "../../img/team/hm-ryan-s.jpg";
-import serena from "../../img/team/hm-serena-w.jpg";
-import thanh from "../../img/team/hm-thanh-n.jpg";
-import victoria from "../../img/team/hm-victoria-m.jpg";
 
+/* Cute Puppies */
+import cute_pup1 from "../../img/team/2022/cute-puppies/cute_pup1.jfif"
+import cute_pup2 from "../../img/team/2022/cute-puppies/cute_pup2.jfif"
+import cute_pup3 from "../../img/team/2022/cute-puppies/cute_pup3.jfif"
+import cute_pup4 from "../../img/team/2022/cute-puppies/cute_pup4.jfif"
+import cute_pup5 from "../../img/team/2022/cute-puppies/cute_pup5.png"
+import cute_pup6 from "../../img/team/2022/cute-puppies/cute_pup6.jfif"
+import cute_pup7 from "../../img/team/2022/cute-puppies/cute_pup7.jfif"
+import cute_pup8 from "../../img/team/2022/cute-puppies/cute_pup8.jfif"
 
+/* Execs */
+import miskaan from "../../img/team/2022/Execs/hm-miskaan.jpeg";
+import aditya from "../../img/team/2022/Execs/hm-aditya-a.jpg";
+// import tanishq from "../../img/team/2022/Execs/hm-tanishq.jpg";
+
+/* Education Team */
+import ryan from "../../img/team/2022/Education team/hm-ryan.jpg"
+import daniel from "../../img/team/2022/Education team/hm-daniel.JPG"
+import hoan from "../../img/team/2022/Education team/hm-hoan.jpg"
+// import jerry from "../../img/team/2022/Education team/hm-jerry.jpg"
+import xinyu from "../../img/team/2022/Education team/hm-xinyu.jpg"
+
+/* Website Team */
+// import arsam from "../../img/team/2022/Website team/hm-arsam.jpg"
+import florence from "../../img/team/2022/Website team/hm-florence.png"
+
+/* Logistics Team */
+import yupin from "../../img/team/2022/Logistics team/hm-yupin.jpeg"
+import aldrich from "../../img/team/2022/Logistics team/hm-aldrich.jpg"
+// import amy from "../../img/team/2022/Logistics team/hm-amy.jpg"
+import jiahao from "../../img/team/2022/Logistics team/hm-jiahao.jpg"
+// import selena from "../../img/team/2022/Logistics team/hm-selena.jpg"
+import suren from "../../img/team/2022/Logistics team/hm-suren.jpg"
+
+/* Marketing Team */
+import whoisthis from "../../img/team/2022/Marketing team/hm-who-is-this.png"
+
+/* Sponsorship Team */
+// import rishabh from "../../img/team/2022/Sponsorship team/hm-rishabh.jpg"
+import sean from "../../img/team/2022/Sponsorship team/hm-sean.jpg"
+// import kasie from "../../img/team/2022/Sponsorship team/hm-kasie.jpg"
+// import henry from "../../img/team/2022/Sponsorship team/hm-henry.jpg"
+import aaditya_g from "../../img/team/2022/Sponsorship team/hm-aaditya-g.jpg"
+
+/* Student Engagement Team */
+import calvin from "../../img/team/2022/Student Engagement team/hm-calvin.jpg"
+import aditya_d from "../../img/team/2022/Student Engagement team/hm-aditya-d.png"
+import adhiraj from "../../img/team/2022/Student Engagement team/hm-adhiraj.JPG"
+import jamie from "../../img/team/2022/Student Engagement team/hm-jamie.jpg"
 
 function MeetTheTeam() {
   return (
@@ -32,29 +57,44 @@ function MeetTheTeam() {
       <div className="meetTheTeamContainer">
         <h1 className="meetTheTeamTitle">Meet the team</h1>
         <div className="teamPhotos">
-          <Team avatar={ali} name="Ali"/>
-          <Team avatar={apuroop} name="Apuroop"/>
-          <Team avatar={chris} name="Chris"/>
-          <Team avatar={daniel} name="Daniel"/>
-          <Team avatar={deborah} name="Deborah"/>
-          <Team avatar={fiona} name="Fiona"/>
-          <Team avatar={ivan} name="Ivan"/>
-          <Team avatar={jeeng} name="Jeeng"/>
-          <Team avatar={jenny} name="Jenny"/>
-          <Team avatar={joanne} name="Joanne"/>
-          <Team avatar={jonathan} name="Jonathan"/>
-          <Team avatar={keith} name="Keith"/>
-          <Team avatar={kye} name="Kye"/>
-          <Team avatar={leo} name="Leo"/>
-          <Team avatar={ley} name="Ley"/>
-          <Team avatar={matthew} name="Matthew"/>
-          <Team avatar={muskaan} name="Muskaan"/>
-          <Team avatar={patricia} name="Patricia"/>
-          <Team avatar={richard} name="Richard"/>
-          <Team avatar={ryan} name="Ryan"/>
-          <Team avatar={serena} name="Serena"/>
-          <Team avatar={thanh} name="Thanh"/>
-          <Team avatar={victoria} name="Victoria"/>
+          <Team avatar={miskaan} name="Miskaan Dalmia" position="President"/>
+          <Team avatar={aditya} name="Aditya Ajit" position="Secretary"/>
+          <Team avatar={cute_pup1} name="Tanishq Chawla" position="Treasurer"/>
+
+          <Team avatar={ryan} name="Ryan Samarakoon" position="Education and Website Director"/>
+          <Team avatar={xinyu} name="Chuah Xin Yu" position="Education Officer"/>
+          <Team avatar={daniel} name="Daniel" position="Education Officer"/>
+          <Team avatar={hoan} name="Hoan" position="Education Officer"/>
+          <Team avatar={cute_pup2} name="Jerry Luong" position="Education Officer"/>
+          <Team avatar={cute_pup6} name="Arsam Samadi" position="Website Officer"/>
+          {/* <Team avatar={arsam} name="Arsam Samadi" position="Website Officer"/> */}
+          <Team avatar={florence} name="Florence Tang" position="Website Officer"/>
+
+          <Team avatar={yupin} name="Gan Yu Pin" position="Logistics Director"/>
+          <Team avatar={aldrich} name="Aldrich Chan" position="Logistics Officer"/>
+          <Team avatar={cute_pup3} name="Amy Ly" position="Logistics Officer"/>
+          {/* <Team avatar={amy} name="Amy Ly" position="Logistics Officer"/> */}
+          <Team avatar={jiahao} name="Jia Hao" position="Logistics Officer"/>
+          <Team avatar={cute_pup4} name="Selena Lee" position="Logistics Officer"/>
+          {/* <Team avatar={selena} name="Selena Lee" position="Logistics Officer"/> */}
+          <Team avatar={suren} name="Suren Fernando" position="Logistics Officer"/>
+
+          <Team avatar={whoisthis} name="Who is this?" position="Marketing"/>
+
+          <Team avatar={cute_pup5} name="Rishabh Dhawan" position="Sponsorships Director"/>
+          {/* <Team avatar={rishabh} name="Rishabh Dhawan" position="Sponsorships Director"/> */}
+          <Team avatar={aaditya_g} name="Aaditya Gupta" position="Sponsorships Officer"/>
+          <Team avatar={cute_pup8} name="Henry Do" position="Sponsorships Officer"/>
+          {/* <Team avatar={henry} name="Henry Do" position="Sponsorships Officer"/> */}
+          <Team avatar={cute_pup7} name="Kasie Wang" position="Sponsorships Officer"/>
+          {/* <Team avatar={kasie} name="Kasie Wang" position="Sponsorships Officer"/> */}
+          <Team avatar={sean} name="Sean Chen" position="Sponsorships Officer"/>
+
+          <Team avatar={calvin} name="Calvin Zhang" position="Student Engagement Director"/>
+          <Team avatar={aditya_d} name="Aditya Desu" position="Student Engagement Officer"/>
+          <Team avatar={adhiraj} name="Adhiraj jain" position="Student Engagement Officer"/>
+          <Team avatar={jamie} name="Jamie Toh" position="Student Engagement Officer"/>
+
           {/* <Team avatar={} name=""/> */}
         </div>
       </div>
@@ -65,11 +105,11 @@ function MeetTheTeam() {
 function Team(props) {
   return (
     <div>
-      <img className="profile" src={props.avatar}></img>
-      <p>{props.name}</p>
+      <img className="profile" src={props.avatar} alt=""></img>
+      <h4>{props.name}</h4>
+      <p>{props.position}</p>
     </div>
   )
 }
-
 
 export default MeetTheTeam;
