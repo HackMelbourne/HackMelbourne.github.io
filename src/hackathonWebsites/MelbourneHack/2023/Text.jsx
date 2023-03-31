04b90c802bf82dd6e84962da7efe8a37a7200978
Xin Yu <chuaxc@unimelb.edu.au>
2023-03-31 14:07:04 +1100

    Added Text and Responsive GlobeAndText Container

--- src/hackathonWebsites/MelbourneHack/2023/Text.jsx
+++ src/hackathonWebsites/MelbourneHack/2023/Text.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 const Text = () => {
+  const subHeaderStyle = { fontWeight: '700', fontSize: '5rem' };
+  const blurbTextStyle = { fontWeight: '100', fontSize: '1rem' };
   return (
-    <div>Text</div>
-  )
-}
+    <div>
+      <div style={subHeaderStyle}>Make the Change.</div>
+      <div style={blurbTextStyle}>
+        HackMelbourne, is hosting a hackathon event for university students of any field, centered around
+        sustainability. This event presents a unique opportunity for students to make a change by showcasing their
+        skills and creativity to develop innovative solutions to address pressing global issues. Alongside collaborating
+        with like-minded individuals, mentors, and industry experts, students can participate in workshops, enjoy free
+        food and snacks.
+      </div>
+    </div>
+  );
+};
 
-export default Text
+export default Text;
--- src/hackathonWebsites/MelbourneHack/2023/index.jsx
+++ src/hackathonWebsites/MelbourneHack/2023/index.jsx
@@ -2,14 +2,37 @@ import React from 'react';
 import Globe from './Globe';
 import Header from './Header';
 import Text from './Text';
+import './index.css';
 
 const MelbourneHack2023 = () => {
+  // Set flex-basis to 50% for both items
+  const itemStyle = {
+    flexBasis: '50%',
+    boxSizing: 'border-box',
+  };
+
+  // Style the Globe and Text components
+  const GlobeStyle = {
+    ...itemStyle,
+    paddingRight: '1rem',
+  };
+  const TextStyle = {
+    ...itemStyle,
+    paddingLeft: '1rem',
+  };
+
   return (
     <div>
       MelbourneHack2023
       <Header />
-      <Globe />
-      <Text />
+      <div className="globeAndTextContainer">
+        <div style={GlobeStyle}>
+          <Globe />
+        </div>
+        <div style={TextStyle}>
+          <Text />
+        </div>
+      </div>
     </div>
   );
 };
