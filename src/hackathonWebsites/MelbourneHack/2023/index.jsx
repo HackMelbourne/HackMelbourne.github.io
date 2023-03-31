a6591e683c4df094cbc17368ac481ed97a6a2ad3
Xin Yu <chuaxc@unimelb.edu.au>
2023-03-31 13:19:40 +1100

    Create Base Components

--- src/hackathonWebsites/MelbourneHack/2023/index.jsx
+++ src/hackathonWebsites/MelbourneHack/2023/index.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import Globe from './Globe';
+import Header from './Header';
+import Text from './Text';
 
 const MelbourneHack2023 = () => {
-  return <div>MelbourneHack2023</div>;
+  return (
+    <div>
+      MelbourneHack2023
+      <Header />
+      <Globe />
+      <Text />
+    </div>
+  );
 };
 
 export default MelbourneHack2023;
