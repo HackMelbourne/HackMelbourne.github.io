5ee5711891796baa67a1a8a07bf45d6b57f49a76
Xin Yu <chuaxc@unimelb.edu.au>
2023-04-03 19:29:59 +1000

    Try to fix ReactRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './routes/root';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import Initiatives from './routes/Initiatives';
 import Sponsor from './routes/Sponsor';
 import Navbar from './components/Navbar';
 import MelbourneHack2023 from './hackathonWebsites/MelbourneHack/2023';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/initiatives',
-    element: <Initiatives />,
-  },
-  {
-    path: '/sponsors',
-    element: <Sponsor />,
-  },
-  {
-    path: '/melbournehack',
-    element: <MelbourneHack2023 />,
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/initiatives" element={<Initiatives />} />
+        <Route path="/sponsors" element={<Sponsor />} />
+        <Route path="/melbournehack" element={<MelbourneHack2023 />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
