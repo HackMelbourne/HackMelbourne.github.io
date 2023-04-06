8f32755c669101baffad84d6664b002be0ec552d
Suren Fernando <suren.fernando2001@gmail.com>
2023-04-07 04:24:48 +1000

    feat: globe component

--- src/hackathonWebsites/MelbourneHack/2023/index.jsx
+++ src/hackathonWebsites/MelbourneHack/2023/index.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
+import styled from "styled-components";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import { Earth } from '../../../components/earth';
+import './globe.css';
+
+const CanvasContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
 const MelbourneHack2023 = () => {
-  return <div>MelbourneHack2023</div>;
+
+//   return <div>MelbourneHack2023</div>;
+
+    return (
+        <CanvasContainer>
+        <Canvas>
+            <Suspense fallback={null}>
+                <Earth/>
+            </Suspense>
+        </Canvas>
+        </CanvasContainer>
+    );
 };
+
 
 export default MelbourneHack2023;
