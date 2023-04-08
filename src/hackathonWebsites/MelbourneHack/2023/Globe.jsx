3246690c1629cf120023dfb6063d5d4d3845948a
Xin Yu (Cindy) <66938425+chuahxinyu@users.noreply.github.com>
2023-04-08 18:56:33 +1000

    Revert "Melbournehack2023 header icon (#10)" (#11)
    This reverts commit c3a39885fc9ae825e52e50d4eb1cb28686576797.

--- src/hackathonWebsites/MelbourneHack/2023/Globe.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Globe = () => {
-  return (
-    <div>Globe</div>
-  )
-}
-
-export default Globe
--- src/hackathonWebsites/MelbourneHack/2023/index.jsx
+++ src/hackathonWebsites/MelbourneHack/2023/index.jsx
@@ -1,45 +1,7 @@
-import React, { useEffect } from 'react';
-import Globe from './Globe';
-import Header from './Header';
-import Text from './Text';
-import './index.css';
-import newIcon from '/three_boxs.ico';
-import useFavicon from '../../../hooks/useFavicon.js';
+import React from 'react';
 
 const MelbourneHack2023 = () => {
-  // Set the favicon
-  useFavicon(newIcon);
-
-  // Set flex-basis to 50% for both items
-  const itemStyle = {
-    flexBasis: '50%',
-    boxSizing: 'border-box',
-  };
-
-  // Style the Globe and Text components
-  const GlobeStyle = {
-    ...itemStyle,
-    paddingRight: '1rem',
-  };
-  const TextStyle = {
-    ...itemStyle,
-    paddingLeft: '1rem',
-  };
-
-  return (
-    <div>
-      <Header />
-      <div className="globeAndTextContainer">
-        <div style={GlobeStyle}>
-          <Globe />
-        </div>
-        <div style={TextStyle}>
-          <Text />
-        </div>
-        <div></div>
-      </div>
-    </div>
-  );
+  return <div>MelbourneHack2023</div>;
 };
 
 export default MelbourneHack2023;
