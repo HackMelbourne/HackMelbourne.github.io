f8db1143b5b104be9598f00f32abf18f5daf8e59
Xin Yu <chuaxc@unimelb.edu.au>
2023-04-20 08:57:01 +1000

    Actually fix the conditional render for navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router';
 
 const dontShowNavbarPathnames = ['/melbournehack', '/hackathon'];
 
 const Layout = () => {
+  const location = useLocation();
   const [isShowNavbar, setIsShowNavbar] = useState(true);
   useEffect(() => {
+    console.log('useEffect!');
     const pathname = window.location.pathname;
     const hash = window.location.hash;
     const isHidden = dontShowNavbarPathnames.some((path) => pathname.includes(path) || hash.includes(path));
     setIsShowNavbar(!isHidden);
-  }, []);
+  }, [location]);
 
   return (
     <>
