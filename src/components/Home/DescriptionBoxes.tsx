8ae66ca705f93e1711ba8d99ab8361a95bac1e70
vjy02 <vyojoh@gmail.com>
2023-11-15 19:41:48 +1100

    Small CSS changes

--- src/components/Home/DescriptionBoxes.tsx
+++ src/components/Home/DescriptionBoxes.tsx
@@ -17,13 +17,13 @@ function DescriptionBoxes({ events }: DescriptionBoxesProps) {
   }
 
   return (
-    <div className="flex flex-col h-[140vh] md:h-[100vh] max-w-[1100px] justify-between w-[80%] m-auto">
+    <div className="flex flex-col h-[140vh] lg:h-[100vh] max-w-[1100px] justify-between w-[80%] m-auto">
 
       {/* First Event */}
-      <div className="flex flex-col h-[30%] md:flex-row md:justify-between md:self-start md:gap-16">
-        <div className="border border-gray-200 h-1/2 md:w-1/2 md:h-full" />
-        <div className="h-1/2 md:h-full md:w-1/2 flex flex-col gap-4 justify-center align-center">
-          <div className="text-2xl md:text-3xl font-bold text-[#F3B52A]">
+      <div className="flex flex-col h-[30%] lg:flex-row lg:justify-between lg:self-start lg:gap-16">
+        <div className="border border-gray-200 h-1/2 lg:w-1/2 lg:h-full" />
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-4 justify-center align-center">
+          <div className="text-xl lg:text-3xl font-bold text-[#F3B52A]">
             Event<span className=""> {events[0].title}</span>
           </div>
           <div className="text-sm">{events[0].description}</div>
@@ -31,21 +31,21 @@ function DescriptionBoxes({ events }: DescriptionBoxesProps) {
       </div>
 
       {/* Second Event */}
-      <div className="flex flex-col h-[30%] md:h-[20%] md:flex-row md:justify-between md:w-4/5 md:self-end md:gap-16">
-        <div className="order-2 md:order-1 h-1/2 md:h-full md:w-1/2 flex flex-col gap-4 justify-center align-center">
-          <div className="text-2xl md:text-3xl font-bold text-[#F3B52A]">
+      <div className="flex flex-col h-[30%] lg:h-[20%] lg:flex-row lg:justify-between lg:w-4/5 lg:self-end lg:gap-16">
+        <div className="order-2 lg:order-1 h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-4 justify-center align-center">
+          <div className="text-xl lg:text-3xl font-bold text-[#F3B52A]">
             Event <span className="">{events[1].title}</span>
           </div>
           <div className="text-sm">{events[1].description}</div>
         </div>
-        <div className="order-1 md:order-2 border h-1/2 md:w-1/2 md:h-full"/>
+        <div className="order-1 lg:order-2 border h-1/2 lg:w-1/2 lg:h-full"/>
       </div>
 
       {/* Third Event */}
-      <div className="flex flex-col h-[30%] md:flex-row md:justify-between md:w-4/5 md:self-start md:gap-16">
-        <div className="border h-1/2 md:w-1/2 md:h-full" />
-        <div className="h-1/2 md:h-full md:w-3/4 flex flex-col gap-4 justify-center align-center">
-          <div className="text-2xl md:text-3xl font-bold text-[#F3B52A]">
+      <div className="flex flex-col h-[30%] lg:flex-row lg:justify-between lg:w-4/5 lg:self-start lg:gap-16">
+        <div className="border h-1/2 lg:w-1/2 lg:h-full" />
+        <div className="h-1/2 lg:h-full lg:w-3/4 flex flex-col gap-4 justify-center align-center">
+          <div className="text-xl lg:text-3xl font-bold text-[#F3B52A]">
             Event<span className=""> {events[2].title}</span>
           </div>
           <div className="text-sm">{events[2].description}</div>
