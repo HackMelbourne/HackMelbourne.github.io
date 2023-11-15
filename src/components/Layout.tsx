d7f320f34eacbd17777f190d1a68eca0f6ce2ab2
Leo Heng <leoheng905@gmail.com>
2023-11-15 22:32:52 +1100

    Changed background color to black

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,23 +2,33 @@ import React, { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
 import { Outlet, useLocation } from 'react-router';
 
-const dontShowNavbarPathnames = ['/melbournehack', '/hackathon'];
+import '../styles/layout.css';
+
+// Pathnames here will not have a navbar
+const dontShowNavbarPathnames = ['/example'];
 
 const Layout = () => {
   const location = useLocation();
   const [isShowNavbar, setIsShowNavbar] = useState(true);
+
+  // Navbar conditional rendering
   useEffect(() => {
     console.log('useEffect!');
     const pathname = window.location.pathname;
-    const hash = window.location.hash;
-    const isHidden = dontShowNavbarPathnames.some((path) => pathname.includes(path) || hash.includes(path));
+    console.log(pathname);
+    const isHidden = dontShowNavbarPathnames.some((path) => pathname.includes(path));
     setIsShowNavbar(!isHidden);
   }, [location]);
 
   return (
     <>
       {isShowNavbar ? <Navbar /> : null}
-      <Outlet />
+      <div className="w-screen max-w-full bg-neutral-950">
+        <Outlet />
+        <ul className="bg-circles">
+          <li></li>
+        </ul>
+      </div>
     </>
   );
 };
