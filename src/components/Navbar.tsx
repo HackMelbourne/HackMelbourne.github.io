ad507c42140a2a64f564c9ba523cf8d654ac6306
Leo Heng <leoheng905@gmail.com>
2023-11-16 00:11:01 +1100

    added gradient border for navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import Logo from '/img/H{Logo.png';
-import { FaDiscord, FaEnvelope, FaFacebook, FaLinkedin, FaBars } from 'react-icons/fa';
-import { DISCORD_LINK, FACEBOOK_LINK, LINKEDIN_LINK, SPONSORSHIP_EMAIL } from '../constants';
 import { Link } from 'react-router-dom';
 
+import '../styles/gradients.css';
+
 const Navbar = () => {
-  return <nav className="max-w-full">hi</nav>;
+  return (
+    <nav className="w-screen max-w-full flex justify-center fixed px-8 pt-8">
+      <div className="grow rounded-md bg-white/20 backdrop-blur-sm gradient-border flex p-2">
+        <div>HackMelbourne</div>
+        <div>Settings</div>
+      </div>
+    </nav>
+  );
 };
 export default Navbar;
