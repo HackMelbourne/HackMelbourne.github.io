d6b920accdc1adf62a9dfdceedf1135a60ba211b
Leo Heng <leoheng905@gmail.com>
2023-11-19 15:18:57 +1100

    Added icons and resized logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu';
 
 // Styles
 import '../styles/gradients.css';
@@ -12,10 +13,12 @@ const Navbar = () => {
     <nav className="w-screen max-w-full flex justify-center fixed px-8 pt-8">
       <div className="grow rounded-md bg-white/20 backdrop-blur-sm gradient-border flex justify-between items-center px-5 py-3">
         <div className="flex items-center">
-          <img src={HM_White_Transparent} className="h-12 w-12"></img>
+          <img src={HM_White_Transparent} className="h-8 w-8"></img>
           HackMelbourne
         </div>
-        <div>Settings</div>
+        <div>
+          <MenuIcon />
+        </div>
       </div>
     </nav>
   );
