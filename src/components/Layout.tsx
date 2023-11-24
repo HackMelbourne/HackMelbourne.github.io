cfa26a3405eb40f299414112bc4ae25d40ac3b8a
Leo Heng <leoheng905@gmail.com>
2023-11-24 19:26:17 +1100

    Changed width css to tailwind for ease of use

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import Navbar from '../components/Navbar';
 import { Outlet, useLocation } from 'react-router';
 
+// Components
+import Navbar from '../components/Navbar';
+
+// Styles
 import '../styles/layout.css';
 
 // Pathnames here will not have a navbar
@@ -26,10 +29,12 @@ const Layout = () => {
       <div className="w-screen max-w-full">
         {isShowNavbar ? <Navbar /> : null}
         <Outlet />
+
+        {/* Background colours */}
         <ul className="w-screen max-w-full bg-circles -z-10 overflow-x-clip">
-          <li></li>
-          <li className="hidden md:block"></li>
-          <li></li>
+          <li className="w-[600px] h-[600px] md:w-[800px] md:h-[800px]"></li>
+          <li className="md:w-[700px] md:h-[700px] hidden md:block"></li>
+          <li className="w-[700px] h-[700px] md:w-[900px] md:h-[900px]"></li>
         </ul>
       </div>
       <div className="w-screen max-w-full h-screen fixed top-0 bg-neutral-950 -z-20"></div>
