a062a67303adf3bbcab5085cdf4ad9c10e8a05df
vjy02 <vyojoh@gmail.com>
2023-12-03 13:17:00 +1100

    Added JoinTheTeam component

---
+++ src/components/Home/JoinTheTeam.tsx
@@ -0,0 +1,28 @@
+import '../../styles/gradients.css';
+
+const JoinTheTeam = () => {
+  return (
+    <div className="join-team-gradient-border flex flex-col min-h-[40vh] rounded-sm md:rounded-[20px] md:min-h-[60vh] max-w-[1100px] w-[100%] md:w-[80%] m-auto items-center justify-center gap-3 md:gap-6">      
+      <div className="text-center text-2xl md:text-5xl font-['Poppins'] font-bold text-white ">
+        Join the <span className="text-primary">Hack</span>
+        Melbourne
+      </div>
+      <div className="text-2xl md:text-5xl font-['Poppins'] font-bold text-white ">
+        team for 2024
+      </div>
+      <div className="text-sm text-center font-['Poppins']">
+        Be make an impact on our future and learn some
+        <br />
+        more skills along the way!
+      </div>
+      {/* Copied button styling from BannerStandard for now */}
+      <div
+        className="text-sm flex w-[120px] h-[45px] py-2.5 px-[15px] justify-center items-center gap-2.5
+          rounded-[5px] bg-primary bg-opacity-40 border-primary border-[1px]">
+        Apply Now
+      </div>
+    </div>
+  );
+};
+
+export default JoinTheTeam;
