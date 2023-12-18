3ea537d9d3b7fa7a57f461a3e9c2afbd0fa8498a
vjy02 <vyojoh@gmail.com>
2023-12-18 20:00:07 +1100

    Added JoinTheTeam in view animation

--- src/features/JoinTheTeam/JoinTheTeam.tsx
+++ src/features/JoinTheTeam/JoinTheTeam.tsx
@@ -1,8 +1,15 @@
 import '../../styles/gradients.css';
+import { motion } from "framer-motion";
 
 const JoinTheTeam = () => {
   return (
-    <div className="join-team-gradient-border flex flex-col min-h-[40vh] rounded-sm md:rounded-[20px] md:min-h-[60vh] max-w-[1100px] w-[100%] md:w-[80%] m-auto items-center justify-center gap-3 md:gap-6">      
+    <motion.div
+      className="join-team-gradient-border flex flex-col min-h-[40vh] rounded-sm md:rounded-[20px] md:min-h-[60vh] max-w-[1100px] w-[100%] md:w-[80%] m-auto items-center justify-center gap-3 md:gap-6"
+      initial={{ opacity:0 }}  
+      transition={{ duration: 3 }} 
+      whileInView={{ opacity: 1}}
+      viewport={{ once: true }}
+    >      
       <div className="text-center text-2xl md:text-5xl font-bold text-white ">
         Join the <span className="text-primary">Hack</span>
         Melbourne
@@ -21,7 +28,7 @@ const JoinTheTeam = () => {
           rounded-[5px] bg-primary bg-opacity-40 border-primary border-[1px]">
         Apply Now
       </div>
-    </div>
+    </motion.div>
   );
 };
 
