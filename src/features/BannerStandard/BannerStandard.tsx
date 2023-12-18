8bd70770e7f4a93ccfb54d6539da9df8bd101ef1
vjy02 <vyojoh@gmail.com>
2023-12-18 20:06:55 +1100

    Added BannerStandard animations

--- src/features/BannerStandard/BannerStandard.tsx
+++ src/features/BannerStandard/BannerStandard.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 interface BannerInfo {
   img: string;
@@ -12,12 +13,25 @@ function BannerStandard({ img, title, desc, button, link }: BannerInfo) {
   return (
     <div
       className="flex w-3/4 min-w-[200px] max-w-[440px] md:w-4/5 md:min-w-[825px] md:max-w-[1100px] md:h-[505px]
-        flex-col md:flex-row-reverse md:justify-between items-center gap-2.5 md:gap-0 m-auto">
-      <div className="md:w-[440px] md:h-[505px] shrink-0 self-stretch">
+        flex-col md:flex-row-reverse md:justify-between items-center gap-2.5 md:gap-0 m-auto"
+    >
+      <motion.div 
+        className="md:w-[440px] md:h-[505px] shrink-0 self-stretch"
+        initial={{ opacity:0 }}  
+        transition={{ duration: 2 }} 
+        whileInView={{ opacity: 1}}
+        viewport={{ once: true }}
+      >  
         <img src={img} />
-      </div>
+      </motion.div>
 
-      <div className="flex flex-col items-center md:items-start gap-5 self-stretch md:self-auto">
+      <motion.div 
+        className="flex flex-col items-center md:items-start gap-5 self-stretch md:self-auto"
+        initial={{ opacity:0 }}  
+        transition={{ delay:1,duration: 2 }} 
+        whileInView={{ opacity: 1}}
+        viewport={{ once: true }}
+      >
         <div>
           <p
             id="hoodie-title"
@@ -37,7 +51,7 @@ function BannerStandard({ img, title, desc, button, link }: BannerInfo) {
                 rounded-[5px] bg-[#F3B52A] bg-opacity-40 border-[#F3B52A] border-[1px]">
           <p className="text-base font-bold">{button}</p>
         </Link>
-      </div>
+      </motion.div>
     </div>
   );
 }
