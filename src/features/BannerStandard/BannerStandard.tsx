07ff3e491785b3ddb4d2bf22a12146efa4009081
Leo Heng <leoheng905@gmail.com>
2023-12-19 13:45:51 +1100

    Added stagger on banner standard

--- src/features/BannerStandard/BannerStandard.tsx
+++ src/features/BannerStandard/BannerStandard.tsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 interface BannerInfo {
   img: string;
@@ -10,49 +10,66 @@ interface BannerInfo {
 }
 
 function BannerStandard({ img, title, desc, button, link }: BannerInfo) {
+  // Animation Settings
+  const container = {
+    hidden: {},
+    show: {
+      transition: {
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const item = {
+    hidden: (i: number) => ({
+      opacity: 0,
+      x: i * 30,
+    }),
+    show: {
+      opacity: 1,
+      x: -0,
+      transition: {
+        duration: 0.4,
+      },
+    },
+  };
+
   return (
-    <div
+    <motion.div
       className="flex w-3/4 min-w-[200px] max-w-[440px] md:w-4/5 md:min-w-[825px] md:max-w-[1100px] md:h-[505px]
         flex-col md:flex-row-reverse md:justify-between items-center gap-2.5 md:gap-0 m-auto"
-    >
-      <motion.div 
-        className="md:w-[440px] md:h-[505px] shrink-0 self-stretch"
-        initial={{ opacity:0 }}  
-        transition={{ duration: 2 }} 
-        whileInView={{ opacity: 1}}
-        viewport={{ once: true }}
-      >  
+      variants={container}
+      viewport={{ amount: 0.8, once: true }}
+      initial="hidden"
+      whileInView="show">
+      <motion.div className="md:w-[440px] md:h-[505px] shrink-0 self-stretch" variants={item} custom={1}>
         <img src={img} />
       </motion.div>
 
-      <motion.div 
-        className="flex flex-col items-center md:items-start gap-5 self-stretch md:self-auto"
-        initial={{ opacity:0 }}  
-        transition={{ delay:1,duration: 2 }} 
-        whileInView={{ opacity: 1}}
-        viewport={{ once: true }}
-      >
-        <div>
+      <div className="flex flex-col items-center md:items-start gap-5 self-stretch md:self-auto">
+        <motion.div variants={item} custom={-1}>
           <p
             id="hoodie-title"
             className="self-stretch md:self-auto text-center md:text-left text-4xl md:text-5xl font-bold">
             {title}
           </p>
-        </div>
+        </motion.div>
 
-        <div id="hoodie-desc" className="md:w-3/4">
+        <motion.div id="hoodie-desc" className="md:w-3/4" variants={item} custom={-1}>
           <p className="self-stretch md:self-auto text-center md:text-left text-base font-medium">{desc}</p>
-        </div>
+        </motion.div>
 
-        <Link
-          to={link}
-          id="hoodie-button"
-          className="flex w-[120px] h-[45px] py-2.5 px-[15px] justify-center items-center gap-2.5
+        <motion.div variants={item} custom={-1}>
+          <Link
+            to={link}
+            id="hoodie-button"
+            className="flex w-[120px] h-[45px] py-2.5 px-[15px] justify-center items-center gap-2.5
                 rounded-[5px] bg-[#F3B52A] bg-opacity-40 border-[#F3B52A] border-[1px]">
-          <p className="text-base font-bold">{button}</p>
-        </Link>
-      </motion.div>
-    </div>
+            <p className="text-base font-bold">{button}</p>
+          </Link>
+        </motion.div>
+      </div>
+    </motion.div>
   );
 }
 
