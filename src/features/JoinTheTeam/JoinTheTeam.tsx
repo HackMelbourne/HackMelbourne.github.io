57588f1692be6d4baf1b60fd4805c34c2d5a9cb7
Leo Heng <leoheng905@gmail.com>
2023-12-19 14:01:40 +1100

    Updated join the team

--- src/features/JoinTheTeam/JoinTheTeam.tsx
+++ src/features/JoinTheTeam/JoinTheTeam.tsx
@@ -1,33 +1,53 @@
 import '../../styles/gradients.css';
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
 const JoinTheTeam = () => {
+  // Animation settings
+  const container = {
+    hidden: {},
+    show: {
+      transition: {
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const item = {
+    hidden: {
+      opacity: 0,
+      y: 30,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.4,
+      },
+    },
+  };
+
   return (
     <motion.div
       className="join-team-gradient-border flex flex-col min-h-[40vh] rounded-sm md:rounded-[20px] md:min-h-[60vh] max-w-[1100px] w-[100%] md:w-[80%] m-auto items-center justify-center gap-3 md:gap-6"
-      initial={{ opacity:0 }}  
-      transition={{ duration: 3 }} 
-      whileInView={{ opacity: 1}}
-      viewport={{ once: true }}
-    >      
-      <div className="text-center text-2xl md:text-5xl font-bold text-white ">
-        Join the <span className="text-primary">Hack</span>
-        Melbourne
-      </div>
-      <div className="text-2xl md:text-5xl font-bold text-white">
-        team for 2024
-      </div>
-      <div className="text-sm text-center">
+      variants={container}
+      viewport={{ amount: 0.8, once: true }}
+      initial="hidden"
+      whileInView="show">
+      <motion.div className="text-center text-2xl md:text-5xl font-bold text-white max-w-2xl" variants={item}>
+        Join the <span className="text-primary">Hack</span>Melbourne team for 2024
+      </motion.div>
+      <motion.div className="text-sm text-center" variants={item}>
         Be make an impact on our future and learn some
         <br />
         more skills along the way!
-      </div>
+      </motion.div>
       {/* Copied button styling from BannerStandard for now */}
-      <div
+      <motion.div
         className="text-sm flex w-[120px] h-[45px] py-2.5 px-[15px] justify-center items-center gap-2.5
-          rounded-[5px] bg-primary bg-opacity-40 border-primary border-[1px]">
+          rounded-[5px] bg-primary bg-opacity-40 border-primary border-[1px]"
+        variants={item}>
         Apply Now
-      </div>
+      </motion.div>
     </motion.div>
   );
 };
