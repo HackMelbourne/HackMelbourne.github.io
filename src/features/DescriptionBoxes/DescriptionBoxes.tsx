bbcba39ac0e980160c54a74031c5a7f603793003
Leo Heng <leoheng905@gmail.com>
2023-12-19 12:35:42 +1100

    Updated animation for description boxes
    added stagger effect
    made animation trigger based on whole component rather than on an individual level

--- src/features/DescriptionBoxes/DescriptionBoxes.tsx
+++ src/features/DescriptionBoxes/DescriptionBoxes.tsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion"
+import { motion } from 'framer-motion';
 
 // Edit span tags to white after bg change to black
 interface Event {
@@ -11,6 +11,31 @@ interface DescriptionBoxesProps {
 }
 
 function DescriptionBoxes({ events }: DescriptionBoxesProps) {
+  // Animation properties
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const item = {
+    hidden: (i: number) => ({
+      opacity: 0,
+      x: i * 30,
+    }),
+    show: {
+      opacity: 1,
+      x: -0,
+      transition: {
+        duration: 0.3,
+      },
+    },
+  };
+
   // Ensure exactly three events
   if (events.length !== 3) {
     console.error('DescriptionBoxes component expects exactly three events.');
@@ -18,58 +43,50 @@ function DescriptionBoxes({ events }: DescriptionBoxesProps) {
   }
 
   return (
-    <div className="flex flex-col h-[140vh] lg:h-[100vh] max-w-[1100px] justify-between w-[80%] m-auto">
+    <motion.div
+      className=" mt-8 flex flex-col h-[140vh] lg:h-[100vh] max-w-[1100px] justify-between w-[80%] m-auto"
+      variants={container}
+      initial="hidden"
+      whileInView="show">
       {/* First Event */}
       <div className="flex flex-col h-[30%] lg:flex-row lg:justify-between lg:self-start lg:gap-16">
         <div className="border border-gray-200 h-1/2 lg:w-1/2 lg:h-full" />
-        <motion.div
-          className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-4 justify-center align-center"
-          initial={{ x: "10vw", opacity:0 }}  
-          transition={{ duration: 1 }} 
-          whileInView={{ opacity: 1, x:0}}
-          viewport={{ once: true }}
-        >
-          <div className="text-xl lg:text-3xl font-bold text-primary">
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-4 justify-center align-center">
+          <motion.div className="text-xl lg:text-3xl font-bold text-primary" variants={item} custom={-1}>
             <span className=""> {events[0].title}</span>
-          </div>
-          <div className="text-sm">{events[0].description}</div>
-        </motion.div>
+          </motion.div>
+          <motion.div className="text-sm" variants={item} custom={-1}>
+            {events[0].description}
+          </motion.div>
+        </div>
       </div>
 
       {/* Second Event */}
       <div className="flex flex-col h-[30%] lg:h-[20%] lg:flex-row lg:justify-between lg:w-4/5 lg:self-end lg:gap-16">
-        <motion.div 
-          className="order-2 lg:order-1 h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-4 justify-center align-center"
-          initial={{ x: "-10vw", opacity:0 }}  
-          transition={{ duration: 1 }} 
-          whileInView={{ opacity: 1, x:0}}
-          viewport={{ once: true }}
-        >
-          <div className="text-xl lg:text-3xl font-bold text-primary">
+        <div className="order-2 lg:order-1 h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-4 justify-center align-center">
+          <motion.div className="text-xl lg:text-3xl font-bold text-primary" variants={item} custom={1}>
             <span className="">{events[1].title}</span>
-          </div>
-          <div className="text-sm">{events[1].description}</div>
-        </motion.div>
+          </motion.div>
+          <motion.div className="text-sm" variants={item} custom={1}>
+            {events[1].description}
+          </motion.div>
+        </div>
         <div className="order-1 lg:order-2 border h-1/2 lg:w-1/2 lg:h-full" />
       </div>
 
       {/* Third Event */}
       <div className="flex flex-col h-[30%] lg:flex-row lg:justify-between lg:w-4/5 lg:self-start lg:gap-16">
         <div className="border h-1/2 lg:w-1/2 lg:h-full" />
-        <motion.div 
-          className="h-1/2 lg:h-full lg:w-3/4 flex flex-col gap-4 justify-center align-center"
-          initial={{ x: "10vw", opacity:0 }}  
-          transition={{ duration: 1 }} 
-          whileInView={{ opacity: 1, x:0}}
-          viewport={{ once: true }}
-        >
-          <div className="text-xl lg:text-3xl font-bold text-primary">
+        <div className="h-1/2 lg:h-full lg:w-3/4 flex flex-col gap-4 justify-center align-center">
+          <motion.div className="text-xl lg:text-3xl font-bold text-primary" variants={item} custom={-1}>
             <span className=""> {events[2].title}</span>
-          </div>
-          <div className="text-sm">{events[2].description}</div>
-        </motion.div>
+          </motion.div>
+          <motion.div className="text-sm" variants={item} custom={-1}>
+            {events[2].description}
+          </motion.div>
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
