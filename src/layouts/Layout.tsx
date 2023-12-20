c9a26930187381e612a46edb65cd46c6091b6dc5
Abhijeet Kumar <abhijeetkumar12346@gmail.com>
2023-12-20 09:51:10 +0000

    update Footer component

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,24 +3,30 @@ import { Outlet, useLocation } from 'react-router';
 
 // Components
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 // Styles
 import '../styles/layout.css';
 
 // Pathnames here will not have a navbar
 const dontShowNavbarPathnames = ['/example'];
+// Pathnames here will not have a footer
+const dontShowFooterPathnames = ['/example'];
 
 const Layout = () => {
   const location = useLocation();
   const [isShowNavbar, setIsShowNavbar] = useState(true);
+  const [isShowFooter, setIsShowFooter] = useState(true);
 
   // Navbar conditional rendering
   useEffect(() => {
     console.log('useEffect!');
     const pathname = window.location.pathname;
     console.log(pathname);
-    const isHidden = dontShowNavbarPathnames.some((path) => pathname.includes(path));
-    setIsShowNavbar(!isHidden);
+    const isNavbarHidden = dontShowNavbarPathnames.some((path) => pathname.includes(path));
+    const isFooterHidden = dontShowFooterPathnames.some((path) => pathname.includes(path));
+    setIsShowNavbar(!isNavbarHidden);
+    setIsShowFooter(!isFooterHidden);
   }, [location]);
 
   const nav = {
@@ -34,13 +40,17 @@ const Layout = () => {
     <>
       {' '}
       <div className="w-screen max-w-full">
-        {isShowNavbar ? <Navbar 
+        {isShowNavbar ? <Navbar
           clubname={nav.clubname}
           pages={nav.pages}
           links={nav.links}
           exit={nav.exit}
         /> : null}
         <Outlet />
+        {isShowFooter ? <Footer
+          links={footer.links}
+          icons={footer.icons}
+        /> : null}
 
         {/* Background colours */}
         <ul className="w-screen max-w-full bg-circles -z-10 overflow-x-clip">
