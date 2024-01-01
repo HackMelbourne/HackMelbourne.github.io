58c8ab191ee20f2431a8ea6f4d5a0b6a79c62da8
Abhijeet Kumar <abhijeetkumar12346@gmail.com>
2024-01-01 11:51:08 +0000

    better eventItem

--- src/features/EventItem/EventItem.tsx
+++ src/features/EventItem/EventItem.tsx
@@ -13,17 +13,29 @@ interface EventItemProps {
 const EventItem = ({ name, date, brief, description, bgImage, frontImage, link }: EventItemProps) => {
 
   return (
-    <div className="bg-cover rounded-[10px] bg-center flex flex-col mx-auto w-4/5 max-w-[800px] min-h-[200px] mb-10" style={{ backgroundImage: `url(${bgImage})` }}>
-      <img
-        className="h-48 rounded-[10px] object-cover w-[20%]"
-        src={frontImage}
-        alt={name}
-      />
-      <div className="p-4">
-        <h2 className="text-xl font-bold">{name}</h2>
-        <p className="text-sm text-gray-600">{date}</p>
-        <p className="text-sm text-gray-600">{brief}</p>
-        <p className="text-sm text-gray-600">{description}</p>
+    <div className="rounded-[10px] mx-auto w-4/5 max-w-[800px] mb-10 min-h-[200px]">
+      <div className="h-[30vw] bg-cover bg-center rounded-[10px]" style={{ backgroundImage: `url(${bgImage})` }}></div>
+      <div className="flex">
+        <div className="w-[40%] px-10">
+          <img
+            className="h-[120%] -mt-[25%] rounded-[10px] object-cover w-full"
+            src={frontImage}
+            alt={name}
+          />
+        </div>
+        <div className="p-4 flex flex-col justify-between w-[60%]">
+          <div>
+            <h2 className="text-4xl font-bold mb-2">{name}</h2>
+            <div className="flex gap-2 mb-2">
+              <p className="font-bold text-xs">{date}</p>
+              <p className="text-gray-600 text-xs">{brief}</p>
+            </div>
+            <p className="text-sm text-gray-600 overflow-y-hidden max-h-[40%] min-h-[40%] h-[10vh] truncate">{description}</p>
+          </div>
+          <a href={link} className="w-[30%] text-center inline-block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300">
+            Visit here
+          </a>
+        </div>
       </div>
     </div>
   );
