f52a5cd5348f245b587077c1e0b28118e7880081
Abhijeet Kumar <abhijeetkumar12346@gmail.com>
2024-01-02 09:33:54 +0000

    made EventItem component mobile responsive

--- src/features/EventItem/EventItem.tsx
+++ src/features/EventItem/EventItem.tsx
@@ -14,28 +14,37 @@ const EventItem = ({ name, date, brief, description, bgImage, frontImage, link }
 
   return (
     <div className="rounded-[10px] mx-auto w-4/5 max-w-[800px] my-20 min-h-[200px]">
-      <div className="h-[30vw] bg-cover bg-center rounded-[10px]" style={{ backgroundImage: `url(${bgImage})` }}></div>
+      {/* small screen heading */}
+      <h2 className="text-4xl font-bold mb-2 md:hidden block text-center">{name}</h2>
+      <div className="md:ml-0 ml-6 h-[30vw] bg-cover bg-center rounded-[10px]" style={{ backgroundImage: `url(${bgImage})` }}></div>
       <div className="flex">
-        <div className="w-[40%] p-4">
+        <div className="w-[60%] md:w-[40%] md:p-4">
           <img
             className="h-[120%] relative -top-[20%] rounded-[10px] object-cover w-full"
             src={frontImage}
             alt={name}
           />
         </div>
-        <div className="p-4 flex flex-col justify-between w-[60%]">
+        <div className="md:p-4 p-2 pr-0 flex flex-col justify-between w-[40%] md:w-[60%] ">
           <div>
-            <h2 className="text-4xl font-bold mb-2">{name}</h2>
-            <div className="flex gap-2 mb-2">
-              <p className="font-bold text-xs">{date}</p>
-              <p className="text-gray-600 text-xs">{brief}</p>
+            <h2 className="text-4xl font-bold mb-2 hidden md:block">{name}</h2>
+            <div className="flex flex-col md:flex-row gap-2 mb-2 md:text-xs sm:text-xl text-base">
+              <p className="font-bold">{date}</p>
+              <p className="text-neutral-400">{brief}</p>
             </div>
-            <p className="text-sm text-gray-600 overflow-y-hidden min-h-[40%] mb-2">{description}</p>
+            <p className="text-sm overflow-y-hidden min-h-[40%] mb-2 hidden md:block">{description}</p>
           </div>
-          <a href={link} className="w-[30%] text-center inline-block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300">
+          <a href={link} className="hidden md:block w-[30%] text-center mt-4 py-3 rounded-md bg-blue-600/20 border border-blue-500 font-semibold">
             Visit here
           </a>
         </div>
+      </div>
+      {/* small screen items */}
+      <div className="p-4 flex flex-col items-center justify-between w-[100%]">
+        <p className="md:hidden block text-sm overflow-y-hidden min-h-[40%] mb-2">{description}</p>
+        <a href={link} className="md:hidden block w-[60%] text-center mt-4 py-3 rounded-md bg-blue-600/20 border border-blue-500 font-semibold">
+          Visit here
+        </a>
       </div>
     </div>
   );
