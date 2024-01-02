f16dd7728213b853b875cbdce091aee4a57a586f
Leo Heng <leoheng905@gmail.com>
2024-01-02 20:02:05 +1100

    Updated color theme for tailwind
    Yellow now has range between 50 to 900

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,20 @@ export default {
       neutral: colors.neutral,
       blue: colors.sky,
       red: colors.red,
-      yellow: '#F3B52A',
-      primary: '#F3B52A',
+      green: colors.green,
+      yellow: {
+        50: '#fef8ea',
+        100: '#fcedca',
+        200: '#fae1aa',
+        300: '#f8d68a',
+        400: '#f7cb6a',
+        500: '#f5c04a',
+        600: '#f3b52a',
+        700: '#cf9a24',
+        800: '#aa7f1d',
+        900: '#866417',
+      },
+      primary: colors.yellow[600],
     },
 
     fontFamily: {
