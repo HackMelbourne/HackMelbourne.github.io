6fe69942a4bc35b9e5dfd3b26ebfabae85237acf
vjy02 <vyojoh@gmail.com>
2024-01-22 21:58:39 +1100

    Changed back font size

--- src/features/DescriptionBoxes/DescriptionBoxes.tsx
+++ src/features/DescriptionBoxes/DescriptionBoxes.tsx
@@ -46,10 +46,10 @@ function DescriptionBoxes({ events }: DescriptionBoxesProps) {
   return (
 <div className="mt-8 flex flex-col gap-[1rem] h-[175vh] lg:min-h-[100vh] lg:h-[100vh] max-w-[1100px] lg:justify-between w-[80%] m-auto">
       {/* First Event */}
-      <div className="flex flex-col h-[32%] max-h-[32%] justify-center lg:flex-row lg:justify-between lg:self-start lg:gap-16">
+      <div className="flex flex-col h-[30%] justify-center lg:flex-row lg:justify-between lg:self-start lg:gap-16">
         <img src={events[0].image} className="object-cover border border-gray-200 h-1/3 lg:w-1/2 lg:h-full rounded-sm" />
         <motion.div
-          className="min-h-3/4 lg:h-full lg:w-1/2 flex flex-col mt-2 lg:gap-4 lg:justify-center justify-between align-center"
+          className="min-h-1/4 lg:h-full lg:w-1/2 flex flex-col mt-2 lg:gap-4 lg:justify-center justify-between align-center"
           variants={container}
           viewport={{ amount: 'all', once: true }}
           initial="hidden"
@@ -57,14 +57,14 @@ function DescriptionBoxes({ events }: DescriptionBoxesProps) {
           <motion.div className="text-xl lg:text-3xl font-bold text-yellow-600" variants={item} custom={-1}>
             <span className=""> {events[0].title}</span>
           </motion.div>
-          <motion.div className="text-sm md:text-base" variants={item} custom={-1}>
+          <motion.div className="text-sm" variants={item} custom={-1}>
             {events[0].description}
           </motion.div>
         </motion.div>
       </div>
 
       {/* Second Event */}
-      <div className="flex flex-col h-[30%] max-h-[30%] justify-center lg:h-[20%] lg:flex-row lg:justify-between lg:w-4/5 lg:self-end lg:gap-16">
+      <div className="flex flex-col h-[30%]  justify-center lg:h-[20%] lg:flex-row lg:justify-between lg:w-4/5 lg:self-end lg:gap-16">
         <motion.div
           className="order-2 lg:order-1 min-h-3/4 mt-2 lg:h-full lg:w-1/2 flex flex-col lg:gap-4 lg:justify-center justify-between align-center"
           variants={container}
@@ -74,7 +74,7 @@ function DescriptionBoxes({ events }: DescriptionBoxesProps) {
           <motion.div className="text-xl lg:text-3xl font-bold text-yellow-600" variants={item} custom={1}>
             <span className="">{events[1].title}</span>
           </motion.div>
-          <motion.div className="text-sm md:text-base" variants={item} custom={1}>
+          <motion.div className="text-sm" variants={item} custom={1}>
             {events[1].description}
           </motion.div>
         </motion.div>
@@ -82,7 +82,7 @@ function DescriptionBoxes({ events }: DescriptionBoxesProps) {
       </div>
 
       {/* Third Event */}
-      <div className="flex flex-col h-[32%] max-h-[32%] justify-center lg:flex-row lg:justify-between lg:w-4/5 lg:self-start lg:gap-16">
+      <div className="flex flex-col h-[30%] justify-center lg:flex-row lg:justify-between lg:w-4/5 lg:self-start lg:gap-16">
         <img src={events[2].image} className="object-cover border h-1/3 lg:w-1/2 lg:h-full rounded-sm" />
         <motion.div
           className="min-h-3/4 lg:h-full lg:w-3/4 mt-2 flex flex-col lg:gap-4 lg:justify-center justify-between align-center"
@@ -93,7 +93,7 @@ function DescriptionBoxes({ events }: DescriptionBoxesProps) {
           <motion.div className="text-xl lg:text-3xl font-bold text-yellow-600" variants={item} custom={-1}>
             <span className=""> {events[2].title}</span>
           </motion.div>
-          <motion.div className="text-sm md:text-base" variants={item} custom={-1}>
+          <motion.div className="text-sm" variants={item} custom={-1}>
             {events[2].description}
           </motion.div>
         </motion.div>
