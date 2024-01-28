828417026aa90fda77bd5ca1f8020ccbfdb6082b
Abhijeet Kumar <abhijeetkumar12346@gmail.com>
2024-01-28 14:30:08 +0000

    update teams for mobile responsive and styling
    - use grid for mobile screen
    - add margins between texts
    - made image rounder

--- src/features/TeamComponent/TeamComponent.tsx
+++ src/features/TeamComponent/TeamComponent.tsx
@@ -26,11 +26,11 @@ const TeamComponent = ({ teamName, description, bgColor, borderColor, members }:
         <div className="mx-auto max-w-[800px] w-[80%] my-20 min-h-[200px] align-center justify-center m-20">
             <h2 className="text-4xl font-bold mb-2 text-center">Meet the {teamName}</h2>
             <p className="text-center mb-4">{description}</p>
-            <div className="flex flex-wrap justify-center gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 md:flex flex-wrap justify-center gap-4">
                 {members.map((member, index) => (
                     <div
                         key={index}
-                        className="flex flex-col items-center p-4 border rounded-lg text-center min-h-[350px] max-w-[350px]"
+                        className="flex flex-col items-center p-4 border rounded-lg text-center min-h-[350px]"
                         style={{
                             backgroundColor: bgColor,
                             borderColor: borderColor,
@@ -39,10 +39,10 @@ const TeamComponent = ({ teamName, description, bgColor, borderColor, members }:
                             // https://stackoverflow.com/a/46278350/13298307
                         }}
                     >
-                        <img src={member.imageUrl} alt={member.name} className="w-full h-auto rounded" />
+                        <img src={member.imageUrl} alt={member.name} className="w-full h-auto rounded-[100rem] m-4" />
                         <p className="text-sm">{member.role}</p>
-                        <h3 className="text-xl font-semibold">{member.name}</h3>
-                        <p className="text-sm mb-auto">{member.about}</p>
+                        <h3 className="text-xl-lg md:text-sm font-semibold m-1">{member.name}</h3>
+                        <p className="text-sm mb-auto m-2">{member.about}</p>
                         <div className="flex space-x-2 mt-2">
                             {member.links.map((link, linkIndex) => (
                                 <a key={linkIndex} href={link.url} target="_blank" rel="noopener noreferrer" className="icon p-3 text-blue-500 hover:underline">
