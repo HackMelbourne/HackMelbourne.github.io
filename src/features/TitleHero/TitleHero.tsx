5a501536fe0da3c42a73bbe4c5deddebdc63a122
Leo Heng <leoheng905@gmail.com>
2024-02-11 18:43:51 +1100

    Better CSS approach to centering titleHero

--- src/features/TitleHero/TitleHero.tsx
+++ src/features/TitleHero/TitleHero.tsx
@@ -8,27 +8,17 @@ interface PageHeader {
 const TitleHero = ({ pageTitle, pageDescription }: PageHeader) => {
   const ref = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (ref.current) {
-      console.log(window.screen.width);
-      console.log(ref.current.offsetWidth);
-      let displacement = window.screen.width / 2 - ref.current.offsetWidth / 2;
-      ref.current.style.transform = `translateX(${displacement}px)`;
-      console.log(displacement);
-    }
-  });
-
   return (
     <div className="grid grid-cols-1 grid-rows-1 items-center md:mt-10 relative overflow-clip">
       {/* Background */}
       <div
-        className="col-start-1 row-start-1 absolute -z-10 stroke-width text-9xl font-black md:text-[256px] whitespace-nowrap"
+        className="col-start-1 row-start-1 absolute -z-10 stroke-width text-9xl font-black md:text-[256px] whitespace-nowrap left-1/2"
         style={{
           color: 'transparent',
           WebkitTextStrokeColor: '#767676',
         }}
         ref={ref}>
-        {pageTitle}
+        <div className=" relative -left-1/2">{pageTitle}</div>
       </div>
       {/* Foreground */}
       <div className="col-start-1 row-start-1 pt-48 px-8 j">
