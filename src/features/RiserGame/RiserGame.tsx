0f52ce1a05e5491d749c24f878521e91069642d2
leoheng <leoheng905@gmail.com>
2024-02-13 18:59:53 +1100

    Added timer, attempts

--- src/features/RiserGame/RiserGame.tsx
+++ src/features/RiserGame/RiserGame.tsx
@@ -1,17 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const RiserGame = () => {
+
+    const [isActive, setIsActive] = useState(false);
+    const [time, setTime] = useState(0);
+    const [attempts, setAttempts] = useState(0);
+
+    useEffect(() => {
+        let interval: NodeJS.Timeout;
+
+        if (isActive === true) {
+            // Random number between 1.0 to 2.0
+            // Not sure if this is working
+            const timeGap = 1 + ((Math.floor((Math.random() * 4)) / 10) - 0.15)
+            console.log(timeGap)
+
+            interval = setInterval(() => {
+                setTime((time) => time + 1);
+            }, timeGap);
+        } else {
+            clearInterval(interval!);
+        }
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, [isActive])
+
+    const handleStart = () => {
+        setTime(0);
+        setIsActive(true);
+    }
+
+    const handleEnd = () => {
+        setIsActive(false);
+        setAttempts(attempts + 1);
+    }
+
+    const btnPress = () => {
+        if (attempts < 2) {
+            if (isActive) {
+                handleEnd();
+            } else {
+                handleStart();
+            }
+        } else {
+            alert("You have used all your attempts")
+        }
+    }
+
+    const attemptStyle = (id: number) => {
+        if (id <= attempts) {
+            return " bg-yellow-500";
+        } else {
+            return "bg-transparent";
+        }
+    }
+
     return (
         <div className='mx-auto flex flex-col gap-8 items-center mt-32'>
-            <div className=''>Get to 9000</div>
-            <div className='font-bold text-8xl'>0000</div>
-            <div className='bg-yellow-500/10 border border-yellow-500 rounded-full w-48 h-48 flex content-center justify-center flex-wrap'>
+            <div className=''>Get to 2024</div>
+            <div className='font-bold text-8xl'>{time}</div>
+            <button onClick={btnPress} className='bg-yellow-500/10 border border-yellow-500 rounded-full w-48 h-48 flex content-center justify-center flex-wrap'>
                 <div className=' font-bold text-4xl'>START</div>
-            </div>
+            </button>
             <div className='flex flex-col gap-2 items-center'>
                 <div className='flex flex-row gap-4'>
-                    <div className='border-4 border-yellow-500 rounded-full h-6 w-6 bg-transparent'></div>
-                    <div className='border-4 border-yellow-500 rounded-full h-6 w-6 bg-transparent'></div>
+                    <div className={`border-4 border-yellow-500 rounded-full h-6 w-6 ${attemptStyle(1)}`}></div>
+                    <div className={`border-4 border-yellow-500 rounded-full h-6 w-6 ${attemptStyle(2)}`}></div>
+
                 </div>
                 <p>Attempts</p>
             </div>
