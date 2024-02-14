50462e7e7316b651c0b782e8b5b429557aad40b8
alie-nel <msnguyen63@gmail.com>
2024-02-14 15:55:14 +1100

    create oweek leaderboard

--- src/routes/eventPages/RiserGame.tsx
+++ src/routes/eventPages/RiserGame.tsx
@@ -1,8 +1,30 @@
 import React, { useRef } from 'react';
+import Ranking from '../../features/Leaderboard/Ranking';
 
 export default function RiserGame() {
+  const leaderboard = [
+    {
+      rank: 1,
+      name: 'First Last',
+      score: 7999,
+    },
+    {
+      rank: 2,
+      name: 'First Last',
+      score: 7999,
+    }
+  ];
 
   return (
-    <p>Riser game</p>
+    <div className="w-screen max-w-full pt-28">
+
+      <h2 className="text-4xl text-center font-bold mb-10">Leaderboard</h2>
+      <div className="flex flex-col gap-6">
+        {leaderboard.map((entry) => (
+          <Ranking {...entry} />
+        ))}
+      </div>
+      
+    </div>
   );
 }
