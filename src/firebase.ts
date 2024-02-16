daf776649cdca309e9b564ee4a1996efbafe1a30
Leo Heng <leoheng905@gmail.com>
2024-02-16 17:25:54 +1100

    Made firebase API key private

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,18 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from '@firebase/firestore';
+import { getFunctions } from 'firebase/functions';
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBOadwxsvjJLjV_b6inYObz73BwGFF7vHA',
+  apiKey: import.meta.env.VITE_FIREBASE_KEY,
   authDomain: 'hmwebsite-b89d5.firebaseapp.com',
   projectId: 'hmwebsite-b89d5',
   storageBucket: 'hmwebsite-b89d5.appspot.com',
   messagingSenderId: '441279518479',
-  appId: '1:441279518479:web:63bacc397a4c342b69907a',
+  appId: '1:441279518479:web:9c31320326c4908a69907a',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const functions = getFunctions();
