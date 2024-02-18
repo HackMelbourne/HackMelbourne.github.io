2cc07ac055764622c8a46723d2bee31ceb63dcc1
Leo Heng <leoheng905@gmail.com>
2024-02-18 15:04:11 +1100

    Updated email input type

--- src/routes/eventPages/OWeek.tsx
+++ src/routes/eventPages/OWeek.tsx
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
-import { Button, TextField, FormControl, Checkbox, FormControlLabel } from '@mui/material';
-import TitleHero from '../../features/TitleHero/TitleHero';
-import { motion, useScroll, useTransform } from 'framer-motion';
-import { useRef } from 'react';
-import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
-import { useNavigate } from 'react-router-dom';
-import { RiserUserInput } from './RiserGame.model';
-import Filter from 'bad-words';
-
+import React, { useState } from "react";
+import { Button, TextField, FormControl, Checkbox, FormControlLabel } from "@mui/material";
+import TitleHero from "../../features/TitleHero/TitleHero";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
+import { RiserUserInput } from "./RiserGame.model";
+import Filter from "bad-words";
 
 import Ranking from "../../features/Leaderboard/Ranking";
 import { Leaderboard } from "@mui/icons-material";
@@ -101,35 +100,35 @@ export default function RiserGame() {
     const inputs = {
       firstName,
       lastName,
-      email
-    }
-    const fullName = firstName+lastName
-    const filter = new Filter()
+      email,
+    };
+    const fullName = firstName + lastName;
+    const filter = new Filter();
     // Inputs length check
-    if (Object.values(inputs).filter(input => input.length > 0).length < 3){
-      alert("Please fill in all required details")
+    if (Object.values(inputs).filter((input) => input.length > 0).length < 3) {
+      alert("Please fill in all required details");
     }
     // Profanity check
-    else if (filter.isProfane(fullName) || filter.isProfane(email)){
-      alert("Profanity detected, please delete any inappropriate text.")
+    else if (filter.isProfane(fullName) || filter.isProfane(email)) {
+      alert("Profanity detected, please delete any inappropriate text.");
     }
     // Email Check
-    else if (!email.match(/\S+@\S+\.\S+/)){
-      alert("Invalid email, please enter a valid email.")
+    else if (!email.match(/\S+@\S+\.\S+/)) {
+      alert("Invalid email, please enter a valid email.");
     }
     // Student ID Check
-    else if (!studentId.match(/^\d{7}$/) && studentId.length > 0){
-      alert("Invalid student ID, please recheck you have entered it correctly.")
+    else if (!studentId.match(/^\d{7}$/) && studentId.length > 0) {
+      alert("Invalid student ID, please recheck you have entered it correctly.");
     }
-    // Pass 
-    else{
+    // Pass
+    else {
       const validInput: RiserUserInput = {
         name: fullName,
         email: email,
         studentID: studentId === "" ? "0000000" : studentId,
         HMMember: isMember,
       };
-      navigate('/O-Week/playGame', { state: { ...validInput } });
+      navigate("/O-Week/playGame", { state: { ...validInput } });
     }
   };
 
@@ -171,7 +170,7 @@ export default function RiserGame() {
                 focused
               />
               <TextField
-                type="text"
+                type="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 variant="standard"
