779b0e63cf9dc221b0dbc50bffbcf7602d9d6dc2
Leo Heng <leoheng905@gmail.com>
2024-02-21 09:49:12 +1100

    Updaeted instagram link

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 export const SPONSORSHIP_EMAIL = `sponsors@hack.melbourne`;
 export const SECRETARY_EMAIL = `secretary@hack.melbourne`;
 export const DISCORD_LINK = `https://discord.gg/tEQ5m6ayTV`;
-export const FACEBOOK_LINK = `https://www.facebook.com/hackmelbourne/`;
+export const FACEBOOK_LINK = `https://www.facebook.com/hack.melbourne/`;
 export const LINKEDIN_LINK = `https://www.linkedin.com/company/hackmelbourne/`;
 export const INSTAGRAM_LINK = `https://www.instagram.com/hackmelbourne`;
-export const UMSU_LINK = 'https://umsu.unimelb.edu.au/buddy-up/clubs/clubs-listing/join/hackmelbourne/';
-export const GITHUB_LINK = 'https://github.com/HackMelbourne/HackMelbourne.github.io';
+export const UMSU_LINK = "https://umsu.unimelb.edu.au/buddy-up/clubs/clubs-listing/join/hackmelbourne/";
+export const GITHUB_LINK = "https://github.com/HackMelbourne/HackMelbourne.github.io";
