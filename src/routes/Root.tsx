89ded4dfc9f8db2d91655f577700a3cc49c6226f
Leo Heng <leoheng905@gmail.com>
2024-03-09 22:45:11 +1100

    Updated description order

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -34,6 +34,12 @@ function Root() {
   // Temp event values
   const events = [
     {
+      title: "DecodED",
+      description:
+        "A workshop style education program tailored towards those with limited experience in software development. Aswell as Hackiethon taking place right after the conclusion of DecodED to allow attendees to apply their new knowledge!",
+      image: DecodEdImage,
+    },
+    {
       title: "Hackiethon",
       description:
         "Our rookie hackathon is tailor-made for inexperienced but eager individuals! \n Unlike our main hackathon, it comes with valuable insights into the experience of competing and finding success in such events.",
@@ -43,12 +49,6 @@ function Root() {
       title: "Melbourne Hack",
       description: `Our main competition, brings together students from across the state for a weekend of intense hacking. Scheduled for Semester 2, this annual event provides the opportunity to collaborate with talented peers, connect with industry-leading companies, develop strong technical skills, and win amazing prizes!`,
       image: MelbHackImage,
-    },
-    {
-      title: "DecodED",
-      description:
-        "A workshop style education program tailored towards those with limited experience in software development. Aswell as Hackiethon taking place right after the conclusion of DecodED to allow attendees to apply their new knowledge!",
-      image: DecodEdImage,
     },
   ];
 
