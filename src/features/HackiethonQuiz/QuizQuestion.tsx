cfaa7cb218e29b091fb8af2af8a500a08730ed0e
Leo Heng <leoheng905@gmail.com>
2024-03-18 23:41:38 +1100

    Hides content after selecting option

--- src/features/HackiethonQuiz/QuizQuestion.tsx
+++ src/features/HackiethonQuiz/QuizQuestion.tsx
@@ -3,9 +3,11 @@ import { motion } from "framer-motion";
 import { QuizQuestionModel, QuizQuestionProps } from "../../routes/eventPages/HackiethonQuiz.model";
 
 const QuizQuestion = ({ title, selections, sendValueChange }: QuizQuestionProps) => {
+  const [visible, setVisible] = useState<Boolean>(true);
+
   // Callback function to update parent
   const handleSelection1 = () => {
-    console.log("Handling selection1");
+    setVisible(false);
     sendValueChange({
       value1: selections[0].value1Weight,
       value2: selections[0].value2Weight,
@@ -14,7 +16,7 @@ const QuizQuestion = ({ title, selections, sendValueChange }: QuizQuestionProps)
   };
 
   const handleSelection2 = () => {
-    console.log("Handling selection2");
+    setVisible(false);
     sendValueChange({
       value1: selections[1].value1Weight,
       value2: selections[1].value2Weight,
@@ -22,21 +24,25 @@ const QuizQuestion = ({ title, selections, sendValueChange }: QuizQuestionProps)
     });
   };
 
-  return (
-    <div className="flex flex-col mx-auto h-screen w-full max-w-screen-md px-6 gap-4">
-      <p className="text-2xl font-bold text-center">{title}</p>
+  if (visible) {
+    return (
+      <div className="flex flex-col mx-auto h-screen w-full max-w-screen-md px-6 gap-4">
+        <p className="text-2xl font-bold text-center">{title}</p>
 
-      <img className="w-full h-96"></img>
+        <img className="w-full h-96"></img>
 
-      {/* Displaying each selection for the question at hand */}
-      <div className="w-full p-6 rounded bg-blue-500/10 border border-blue-500" onClick={handleSelection1}>
-        Tell them blah blah
+        {/* Displaying each selection for the question at hand */}
+        <div className="w-full p-6 rounded bg-blue-500/10 border border-blue-500" onClick={handleSelection1}>
+          Tell them blah blah
+        </div>
+        <div className="w-full p-6 rounded bg-yellow-500/10 border border-yellow-500" onClick={handleSelection2}>
+          Tell them blah blah
+        </div>
       </div>
-      <div className="w-full p-6 rounded bg-yellow-500/10 border border-yellow-500" onClick={handleSelection2}>
-        Tell them blah blah
-      </div>
-    </div>
-  );
+    );
+  } else {
+    return <></>;
+  }
 };
 
 export default QuizQuestion;
