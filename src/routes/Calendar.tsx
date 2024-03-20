4600ff1dc45b44713ea47f0d4aeec30db9df353a
vjy02 <vyojoh@gmail.com>
2024-03-20 12:11:59 +1100

    Centered calendar items and removed height limits

--- src/routes/Calendar.tsx
+++ src/routes/Calendar.tsx
@@ -47,7 +47,7 @@ const Calendar = () => {
     ]
 
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 w-[80%] mx-auto max-h-[75vh] md:max-h-[45vh] overflow-y-auto border">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 w-[80%] mx-auto gap-5 border">
             {data.map((eventItem, index) => {
                 return (
                     <CalendarItem 
