3943585071ecebe1b395abb64acade963aaf7595
Leo Heng <leoheng905@gmail.com>
2024-03-20 22:16:09 +1100

    Added hexToRGBA service

---
+++ src/services/hexToRGBA.ts
@@ -0,0 +1,7 @@
+export function hexToRGBA(hex: string, alpha: string) {
+  const r = parseInt(hex.slice(0, 2), 16);
+  const g = parseInt(hex.slice(2, 4), 16);
+  const b = parseInt(hex.slice(4, 6), 16);
+
+  return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+}
