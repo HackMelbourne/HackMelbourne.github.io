df177b0e02031b4604e583b3c6f648e9ed218215
Leo Heng <leoheng905@gmail.com>
2024-03-24 10:09:54 +1100

    Added backend functionality

---
+++ src/features/TournamentBracket/TournamentBracket.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { TournamentBracketProps } from "./TournamentBracket.model";
+
+const TournamentBracket = ({ title, link }: TournamentBracketProps) => {
+  return (
+    <div>
+      <h2 className="font-bold text-2xl">{title}</h2>
+      <iframe src={`${link}/embed/`} width="100%" height="550"></iframe>
+    </div>
+  );
+};
+
+export default TournamentBracket;
--- src/routes/eventPages/Hackiethon.tsx
+++ src/routes/eventPages/Hackiethon.tsx
@@ -1,14 +1,28 @@
+import { useEffect, useState } from "react";
 import TitleHero from "../../features/TitleHero/TitleHero";
+import { getHackiethonBrackets } from "../../services/tournamentServices";
+import { TournamentBracketProps } from "../../features/TournamentBracket/TournamentBracket.model";
+import TournamentBracket from "../../features/TournamentBracket/TournamentBracket";
 
 const Hackiethon = () => {
+  const [brackets, setBrackets] = useState<TournamentBracketProps[]>([]);
+
+  useEffect(() => {
+    getHackiethonBrackets().then((result) => {
+      setBrackets(result);
+      console.log(brackets);
+    });
+  }, []);
+
   return (
     <>
       <TitleHero
         pageTitle="Hackiethon"
         pageDescription="The hackiethon tournament bracket. Best of luck to every participant"></TitleHero>
       <div className="max-w-screen-lg mx-auto mt-24">
-        <h2 className="font-bold text-2xl">Semi Finals Tournament</h2>
-        <iframe src="https://brackethq.com/b/8bczb/embed/" width="100%" height="550"></iframe>
+        {brackets.map((value, key) => {
+          return <TournamentBracket title={value.title} link={value.link} key={key}></TournamentBracket>;
+        })}
       </div>
     </>
   );
