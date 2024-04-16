7265a2b13ba6ff6641e52e5949b8d57ec2802446
Clement Chau <clementtjuwito2@gmail.com>
2024-04-16 22:01:19 +1000

    fix calendar grids

--- src/routes/Calendar.tsx
+++ src/routes/Calendar.tsx
@@ -25,7 +25,7 @@ const Calendar = () => {
           <CircularProgress />
         </div>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 md:gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-screen-xl mx-auto gap-16 md:gap-8 mt-16 px-8">
           {calendar.map((eventItem, index) => (
             <CalendarItem
               key={index}
