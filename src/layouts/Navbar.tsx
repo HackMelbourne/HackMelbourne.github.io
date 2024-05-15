6ffd64bf51f2b3043c7cf296c8dd0d4b1a0fdc33
vjy02 <vyojoh@gmail.com>
2024-05-15 23:05:47 +1000

    feat: update desktop nav and add placeholder boxes

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -40,21 +40,44 @@ const Navbar = ({ clubname, logo, pages, links }: Nav) => {
   const revealMobileNavItem = () => {
     if (activePage == null) return
     return (
-      <Slide in={activePage !== null}>
-        <div className="flex flex-col justify-center items-start h-3/4 w-3/4 m-auto gap-3">
+      <Slide in={activePage !== null}> 
+        <div className="flex flex-col justify-start items-start min-h-screen w-3/4 m-auto gap-3 mt-16">
           <h3 className="text-xl" onClick={()=>{
             revealMobileNav(null);
             setIsSubMenuOpen(true);
           }}>Back</h3>
           <h3 className="text-3xl font-bold bg-white bg-opacity-10 py-2 px-4 w-full rounded-md">{pages[activePage]}</h3>
+          <div className="flex flex-col h-1/3 w-full justify-between">
+            <div className="border-white border w-full h-full rounded-md bg-neutral-700"></div>
+            <div className="border-white border w-full h-full rounded-md bg-neutral-700 mt-2"></div>
+            <div className="border-white border w-full h-full rounded-md bg-neutral-700 mt-2"></div>
+          </div>
         </div>
       </Slide>
     );
   };
 
+  const tempNavItem = () => {
+    if (activePage == null) return
+    return (
+      <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 mt-16 w-fit h-fit"         onMouseLeave={()=>setActivePage(null)}
+      >
+        {/* Really bootleg solution, creates an invisible box to ensure hovered nav item doesnt 
+        dissapear since the items are disconnected from the bar */}
+        <div className="absolute -top-6 transform -translate-x-1/2 h-max w-max py-2">
+          <div className="border-white border w-36 h-14 rounded-md bg-neutral-700"></div>
+          <div className="border-white border w-36 h-14 rounded-md bg-neutral-700 mt-2"></div>
+          <div className="border-white border w-36 h-14 rounded-md bg-neutral-700 mt-2"></div>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="flex justify-center">
-      <nav className="w-screen max-w-[800px] fixed pt-8 z-40 px-8">
+      <nav 
+        className="w-screen max-w-[800px] fixed pt-8 z-40 px-8"
+      >
         <div className="max-w-[800px] grow rounded-md bg-black/20 backdrop-blur-md border flex justify-between items-center px-5 py-3">
           <Link to={links[0]} className="flex items-center justify-start gap-2 font-bold" onClick={handleLinkClick}>
             <img src={logo} className="h-8 w-8"></img>
@@ -67,14 +90,20 @@ const Navbar = ({ clubname, logo, pages, links }: Nav) => {
           {/* DESKTOP */}
           <div className="hidden md:flex justify-end gap-6 items-center">
             {pages.map((page, index) => (
-              <Link
-                key={index}
-                to={links[index]}
-                onClick={handleLinkClick}
-                className=" hover:underline hover:text-primary">
-                {page}
-              </Link>
-            ))}
+                <div
+                  onMouseEnter={()=>setActivePage(index)} 
+                  className="relative"
+                >
+                  <Link
+                    key={index}
+                    to={links[index]}
+                    onClick={()=>setActivePage(index)}
+                    className="hover:underline hover:text-primary">
+                    {page}
+                  </Link>
+                  {activePage === index && tempNavItem()} 
+                </div>
+              ))}
           </div>
         </div>
       </nav>
