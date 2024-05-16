6d94a41e26b685af6474e4648138566f033c62eb
wash <91541609+washNOTaWEBDEV@users.noreply.github.com>
2024-05-16 21:09:32 +1000

    Rerouting

---
+++ src/routes/OurMission.tsx
@@ -0,0 +1,11 @@
+import TitleHero from "../features/TitleHero/TitleHero";
+
+const OurMission = () => {
+  return (
+    <>
+      <TitleHero pageTitle="Our Mission" pageDescription="Page coming soon!" />
+    </>
+  );
+};
+
+export default OurMission;
